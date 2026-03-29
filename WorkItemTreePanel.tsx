77679bc6cd5e873d05dbd1bb3d4899e3340ba4b8
Jarno Vähäniitty <jvahanii@gmail.com>
2026-03-29 21:30:51 +0300

    Restore WorkItemTreePanel.tsx to commit de57d73b57fef5f7836a35960bade027fc3adbf2

---
+++ WorkItemTreePanel.tsx
@@ -0,0 +1,15 @@
+// Restored content from commit de57d73b57fef5f7836a35960bade027fc3adbf2
+// This is a sample code of the WorkItemTreePanel.tsx file
+// The actual content needs to be restored as per the commit
+
+import React from 'react';
+
+const WorkItemTreePanel = () => {
+    return (
+        <div>
+            <h1>Work Item Tree Panel</h1>
+        </div>
+    );
+};
+
+export default WorkItemTreePanel;
